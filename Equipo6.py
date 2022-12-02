d0c409f3991726f020a033261881f0428007d981
Victor <victorng01@hotmail.com>
2022-12-02 09:46:34 -0600

    update orden

--- Equipo6.py
+++ Equipo6.py
@@ -3,78 +3,6 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-st.title("Familias fuera de sus estándares")
-
-option = st.selectbox(
-'Selecciona una variable',
-('RC Temp Average - 1era Posición', 'FC Temp Average - 1era Posición', 'RC Temp Average - 2da Posición', 'FC Temp Average - 2da Posición'))
-st.write('You selected:', option)
-
-current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd
-famstemps = current_dir / "Familias y estandares de temperatura.xlsx"
-#("C:\\Users\\victo\\Desktop\\Everything\\Streamlit\\Familias y estandares de temperatura.xlsx")
-df = pd.read_excel(famstemps)
-
-if option == 'RC Temp Average - 1era Posición':
-    fig = px.pie(df, values = 'PerRC1', names = "Fam", color_discrete_sequence = px.colors.sequential.YlOrBr)
-    st.write(fig)
-    
-    st.subheader("Las 3 familias con mejor desempeño son:")
-    st.write('Fam - 4W3G80: De un total de 117 datos, 8 están fuera de sus estándares.\n' 'El 6.84 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4w3iG81: De un total de 23 datos, 2 están fuera de sus estándares.\n' 'El 8.70 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4w3n90: De un total de 39 datos, 4 están fuera de sus estándares.\n' 'El 10.26 porciento del total de sus datos están fuera de los estándares')
-    
-    st.subheader("Las 3 familias con peor desempeño son:")
-    st.write('Fam - 6w3n80: De un total de 79 datos, 63 están fuera de sus estándares.\n' 'El 79.75 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 6w3g10: De un total de 13 datos, 8 están fuera de sus estándares.\n' 'El 61.54 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4W3G12: De un total de 92 datos, 45 están fuera de sus estándares.\n' 'El 48.91 porciento del total de sus datos están fuera de los estándares')
-
-elif option == 'FC Temp Average - 1era Posición':
-    fig = px.pie(df, values = 'PerFC1', names = "Fam", color_discrete_sequence = px.colors.sequential.YlOrBr)
-    st.write(fig)
-
-    st.subheader("Las 3 familias con mejor desempeño son:")
-    st.write('Fam - 5w3n80: De un total de 54 datos, 21 están fuera de sus estándares.\n' 'El 38.89 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4w3iG81: De un total de 23 datos, 11 están fuera de sus estándares.\n' 'El 47.83 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4W3G80: De un total de 117 datos, 66 están fuera de sus estándares.\n' 'El 56.41 porciento del total de sus datos están fuera de los estándares')
-    
-    st.subheader("Las familias con peor desempeño son:")
-    st.write('Fam - 6w3n80: De un total de 79 datos, 79 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 6w3g10: De un total de 13 datos, 13 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4W3G12: De un total de 92 datos, 92 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 0w3i10: De un total de 12 datos, 12 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4w3n10: De un total de 21 datos, 21 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-
-elif option == 'RC Temp Average - 2da Posición':
-    fig = px.pie(df, values = 'PerRC2', names = "Fam", color_discrete_sequence = px.colors.sequential.YlOrBr)
-    st.write(fig)
-
-    st.subheader("Las 3 familias con mejor desempeño son:")
-    st.write('Fam - 6w3g10: De un total de 13 datos, 8 están fuera de sus estándares.\n' 'El 61.54 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 0w3i10: De un total de 12 datos, 9 están fuera de sus estándares.\n' 'El 75.00 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4W3G12: De un total de 92 datos, 76 están fuera de sus estándares.\n' 'El 82.61 porciento del total de sus datos están fuera de los estándares')
-    
-    st.subheader("Las 3 familias con peor desempeño son:")
-    st.write('Fam - 4w3n10: De un total de 21 datos, 21 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4W3G80: De un total de 117 datos, 117 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4w3iG81: De un total de 23 datos, 23 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-
-else:
-    fig = px.pie(df, values = 'PerFC2', names = "Fam", color_discrete_sequence = px.colors.sequential.YlOrBr)
-    st.write(fig)
-
-    st.subheader("Las 3 familias con mejor desempeño son:")
-    st.write('Fam - 6w3n80: De un total de 79 datos, 35 están fuera de sus estándares.\n' 'El 44.30 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4w3n10: De un total de 21 datos, 13 están fuera de sus estándares.\n' 'El 61.90 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4W3G12: De un total de 92 datos, 58 están fuera de sus estándares.\n' 'El 63.04 porciento del total de sus datos están fuera de los estándares')
-    
-    st.subheader("Las familias con peor desempeño son:")
-    st.write('Fam - 4W3G80: De un total de 117 datos, 117 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4w3iG81: De un total de 23 datos, 23 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4w3n90: De un total de 39 datos, 39 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 5w3n80: De un total de 54 datos, 54 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-    st.write('Fam - 4W3N80: De un total de 43 datos, 43 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
-
 st.title("Análisis de Below Rating Point")
 
 option = st.selectbox(
@@ -161,3 +89,76 @@ elif option == 'FC_Temp_Average - 1era Posición':
     fig = px.box(df, x = 'Familia', y = 'FC Temp Average\nÂ°F (M/M)',color= 'Familia', color_discrete_sequence=["#eeb111", "black"])
     fig.add_hline(y=4, line_color='Red')
     st.write(fig)
+
+
+st.title("Familias fuera de sus estándares")
+
+option = st.selectbox(
+'Selecciona una variable',
+('RC Temp Average - 1era Posición', 'FC Temp Average - 1era Posición', 'RC Temp Average - 2da Posición', 'FC Temp Average - 2da Posición'))
+st.write('You selected:', option)
+
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd
+famstemps = current_dir / "Familias y estandares de temperatura.xlsx"
+#("C:\\Users\\victo\\Desktop\\Everything\\Streamlit\\Familias y estandares de temperatura.xlsx")
+df = pd.read_excel(famstemps)
+
+if option == 'RC Temp Average - 1era Posición':
+    fig = px.pie(df, values = 'PerRC1', names = "Fam", color_discrete_sequence = px.colors.sequential.YlOrBr)
+    st.write(fig)
+    
+    st.subheader("Las 3 familias con mejor desempeño son:")
+    st.write('Fam - 4W3G80: De un total de 117 datos, 8 están fuera de sus estándares.\n' 'El 6.84 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4w3iG81: De un total de 23 datos, 2 están fuera de sus estándares.\n' 'El 8.70 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4w3n90: De un total de 39 datos, 4 están fuera de sus estándares.\n' 'El 10.26 porciento del total de sus datos están fuera de los estándares')
+    
+    st.subheader("Las 3 familias con peor desempeño son:")
+    st.write('Fam - 6w3n80: De un total de 79 datos, 63 están fuera de sus estándares.\n' 'El 79.75 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 6w3g10: De un total de 13 datos, 8 están fuera de sus estándares.\n' 'El 61.54 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4W3G12: De un total de 92 datos, 45 están fuera de sus estándares.\n' 'El 48.91 porciento del total de sus datos están fuera de los estándares')
+
+elif option == 'FC Temp Average - 1era Posición':
+    fig = px.pie(df, values = 'PerFC1', names = "Fam", color_discrete_sequence = px.colors.sequential.YlOrBr)
+    st.write(fig)
+
+    st.subheader("Las 3 familias con mejor desempeño son:")
+    st.write('Fam - 5w3n80: De un total de 54 datos, 21 están fuera de sus estándares.\n' 'El 38.89 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4w3iG81: De un total de 23 datos, 11 están fuera de sus estándares.\n' 'El 47.83 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4W3G80: De un total de 117 datos, 66 están fuera de sus estándares.\n' 'El 56.41 porciento del total de sus datos están fuera de los estándares')
+    
+    st.subheader("Las familias con peor desempeño son:")
+    st.write('Fam - 6w3n80: De un total de 79 datos, 79 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 6w3g10: De un total de 13 datos, 13 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4W3G12: De un total de 92 datos, 92 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 0w3i10: De un total de 12 datos, 12 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4w3n10: De un total de 21 datos, 21 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+
+elif option == 'RC Temp Average - 2da Posición':
+    fig = px.pie(df, values = 'PerRC2', names = "Fam", color_discrete_sequence = px.colors.sequential.YlOrBr)
+    st.write(fig)
+
+    st.subheader("Las 3 familias con mejor desempeño son:")
+    st.write('Fam - 6w3g10: De un total de 13 datos, 8 están fuera de sus estándares.\n' 'El 61.54 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 0w3i10: De un total de 12 datos, 9 están fuera de sus estándares.\n' 'El 75.00 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4W3G12: De un total de 92 datos, 76 están fuera de sus estándares.\n' 'El 82.61 porciento del total de sus datos están fuera de los estándares')
+    
+    st.subheader("Las 3 familias con peor desempeño son:")
+    st.write('Fam - 4w3n10: De un total de 21 datos, 21 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4W3G80: De un total de 117 datos, 117 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4w3iG81: De un total de 23 datos, 23 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+
+else:
+    fig = px.pie(df, values = 'PerFC2', names = "Fam", color_discrete_sequence = px.colors.sequential.YlOrBr)
+    st.write(fig)
+
+    st.subheader("Las 3 familias con mejor desempeño son:")
+    st.write('Fam - 6w3n80: De un total de 79 datos, 35 están fuera de sus estándares.\n' 'El 44.30 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4w3n10: De un total de 21 datos, 13 están fuera de sus estándares.\n' 'El 61.90 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4W3G12: De un total de 92 datos, 58 están fuera de sus estándares.\n' 'El 63.04 porciento del total de sus datos están fuera de los estándares')
+    
+    st.subheader("Las familias con peor desempeño son:")
+    st.write('Fam - 4W3G80: De un total de 117 datos, 117 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4w3iG81: De un total de 23 datos, 23 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4w3n90: De un total de 39 datos, 39 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 5w3n80: De un total de 54 datos, 54 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
+    st.write('Fam - 4W3N80: De un total de 43 datos, 43 están fuera de sus estándares.\n' 'El 100 porciento del total de sus datos están fuera de los estándares')
